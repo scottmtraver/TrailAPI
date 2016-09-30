d20da68a3d037b2e940c513573e79f53afd7bbaa
Scott <scottmtraver@gmail.com>
2016-09-29 20:21:52 -0600

    Update npm packages and add proper sample data for sponsors model

--- resources/sponsor.js
+++ resources/sponsor.js
@@ -7,5 +7,25 @@ jsonApi.define({
   attributes: {
     title: jsonApi.Joi.string(),
     url: jsonApi.Joi.string().uri()
-  }
+  },
+    examples: [
+    {
+      id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
+      type: "sponsors",
+      title: "Matrix Code",
+      url: "http://www.example.com/foobar",
+    },
+    {
+      id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
+      type: "sponsors",
+      title: "Another Code",
+      url: "http://www.example.com/foobar",
+    },
+    {
+      id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
+      type: "sponsors",
+      title: "Final Code",
+      url: "http://www.example.com/foobar",
+    }
+  ]
 });
