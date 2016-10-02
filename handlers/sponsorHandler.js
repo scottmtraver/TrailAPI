ee6678eb55a1b63342193ec524006394895a3483
Scott <scottmtraver@gmail.com>
2016-10-02 12:59:23 -0600

    Configure the first postgres resource handler

--- handlers/sponsorHandler.js
+++ handlers/sponsorHandler.js
@@ -1,4 +1,14 @@
 "use strict";
 var jsonApi = require("jsonapi-server");
+var RelationalDbStore = require("jsonapi-store-relationaldb");
 
-module.exports = new jsonApi.MemoryHandler();
+//module.exports = new jsonApi.MemoryHandler();
+module.exports = new RelationalDbStore({
+    dialect: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "runontrailsdb", // If not provided, defaults to the name of the resource
+    username: "scott",
+    password: "One23456!",
+    logging: true
+  });
--- resources/sponsor.js
+++ resources/sponsor.js
@@ -5,27 +5,27 @@ jsonApi.define({
   resource: "sponsors",
   handlers: sponsorHandler,
   attributes: {
-    title: jsonApi.Joi.string(),
-    url: jsonApi.Joi.string().uri()
+    name: jsonApi.Joi.string(),
+    linkUrl: jsonApi.Joi.string().uri()
   },
     examples: [
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
-      title: "Matrix Code",
-      url: "http://www.example.com/foobar",
+      name: "Matrix Code",
+      linkUrl: "http://www.example.com/foobar",
     },
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
-      title: "Another Code",
-      url: "http://www.example.com/foobar",
+      name: "Another Code",
+      linkUrl: "http://www.example.com/foobar",
     },
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
-      title: "Final Code",
-      url: "http://www.example.com/foobar",
+      name: "Final Code",
+      linkUrl: "http://www.example.com/foobar",
     }
   ]
 });
