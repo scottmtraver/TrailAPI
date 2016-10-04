e175b58cbc26c059f27358f06dcfa4436d953061
Scott <scottmtraver@gmail.com>
2016-10-03 19:05:07 -0600

    Finalize sponsor model fields

--- resources/sponsor.js
+++ resources/sponsor.js
@@ -6,26 +6,34 @@ jsonApi.define({
   handlers: sponsorHandler,
   attributes: {
     name: jsonApi.Joi.string(),
+    text: jsonApi.Joi.string(),
     linkUrl: jsonApi.Joi.string().uri()
+    imageUrl: jsonApi.Joi.string().uri()
   },
     examples: [
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
       name: "Matrix Code",
+      text: "sample text",
       linkUrl: "http://www.example.com/foobar",
+      imageUrl: "http://www.example.com/foobar",
     },
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
       name: "Another Code",
+      text: "sample text",
       linkUrl: "http://www.example.com/foobar",
+      imageUrl: "http://www.example.com/foobar",
     },
     {
       id: "aab14844-97e7-401c-98c8-0bd5ec922d93",
       type: "sponsors",
       name: "Final Code",
+      text: "sample text",
       linkUrl: "http://www.example.com/foobar",
+      imageUrl: "http://www.example.com/foobar",
     }
   ]
 });
