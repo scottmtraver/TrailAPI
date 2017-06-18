fc7bc11fab87cb605d0c289a3cbaa376b757efcf
Scott <scottmtraver@gmail.com>
2017-06-18 15:44:34 -0600

    Add date string to photos

--- resources/photo.js
+++ resources/photo.js
@@ -6,23 +6,27 @@ jsonApi.define({
   handlers: photoHandler,
   attributes: {
     image_url: jsonApi.Joi.string().uri(),
-    is_active: jsonApi.Joi.boolean()
+    is_active: jsonApi.Joi.boolean(),
+    date: jsonApi.Joi.string()
   },
     examples: [
     {
       id: "1",
       image_url: "http://placehold.it/350x150",
-      is_active: true
+      is_active: true,
+      date: '2017-06-21',
     },
     {
       id: "2",
       image_url: "http://placehold.it/350x150",
-      is_active: true
+      is_active: true,
+      date: '2017-06-21'
     },
     {
       id: "3",
       image_url: "http://placehold.it/350x150",
-      is_active: false
+      is_active: false,
+      date: '2017-06-21'
     }
   ]
 });
